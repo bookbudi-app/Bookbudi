2ae72bf7c066036c140a2bd6c5c81cb726efc804
Digi18 <me.digvijay18@gmail.com>
2019-05-16 19:48:28 +0530

    Removed Banner adapter and banners from Home fragment

--- app/src/main/java/com/app/bookbudiapp/Home.java
+++ app/src/main/java/com/app/bookbudiapp/Home.java
@@ -26,15 +26,6 @@ import java.util.List;
 
 public class Home extends Fragment {
 
-
-    ViewPager bannerViewPager;
-    BannerAdapter adapter;
-    List<BannerModel> banners;
-    TabLayout tabLayout;
-    RequestQueue requestQueue;
-    StringRequest stringRequest;
-    private static final String URI = "https://bookbudiapp.herokuapp.com/banners";
-
     public Home() {
         // Required empty public constructor
     }
@@ -46,61 +37,9 @@ public class Home extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        tabLayout = view.findViewById(R.id.tabLayout);
-        bannerViewPager = view.findViewById(R.id.bannerViewpager);
-
-        tabLayout.setupWithViewPager(bannerViewPager, true);
-
-        banners = new ArrayList<>();
-
-        loadViewPager();
 
         return view;
     }
 
-    private void loadViewPager(){
-
-        requestQueue = Volley.newRequestQueue(getActivity());
-
-        stringRequest = new StringRequest(Request.Method.GET,URI, new Response.Listener<String>() {
-
-            @Override
-            public void onResponse(String response) {
-
-                try {
-
-                    JSONArray jsonArray = new JSONArray(response);
-
-                    for(int i = 0;i<jsonArray.length();i++){
-
-                        JSONObject jsonObject = jsonArray.getJSONObject(i);
-
-                        String loadImages = jsonObject.getString("Image");
-
-                        BannerModel model = new BannerModel(loadImages);
-                        banners.add(model);
-
-                    }
-
-                    adapter = new BannerAdapter(banners,getActivity());
-
-                    bannerViewPager.setAdapter(adapter);
-
-                }catch(JSONException e){
-
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-                TastyToast.makeText(getActivity(),"Error:"+error.getMessage(),TastyToast.LENGTH_SHORT,TastyToast.ERROR).show();
-            }
-        });
-
-        requestQueue.add(stringRequest);
-    }
 
 }
